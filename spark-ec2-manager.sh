15a90a9a9ed25f715a5e3f75c7945705ab3782e2
alextbok <alexander_bok@brown.edu>
2014-08-25 19:27:29 -0400

    Added better key file support and the ability to add more cmd line flags

--- spark-ec2-manager.sh
+++ spark-ec2-manager.sh
@@ -1,8 +1,57 @@
 #!/bin/bash
 #spark ec2 cluster management script
 
-export KEY_PAIR_NAME=name_of_your_key_pair
-export KEY_FILE=/path/to/your/key/file
+export KEY_PAIR_NAME=""
+export KEY_FILE=""
+
+function add_param() {
+	if [ -z "$2" ]; then
+		echo "$1"
+	elif [[ "$2" == "w" || \
+			"$2" == "t" || \
+			"$2" == "m" || \
+			"$2" == "r" || \
+			"$2" == "z" || \
+			"$2" == "a" || \
+			"$2" == "v" || \
+			"$2" == "spark-git-repo" || \
+			"$2" == "hadoop-major-version" || \
+			"$2" == "D" || \
+			"$2" == "ebs-vol-size" || \
+			"$2" == "swap" || \
+			"$2" == "spot-price" || \
+			"$2" == "u" || \
+			"$2" == "worker-instances" || \
+			"$2" == "master-ops" ]]; then
+		if [ -z "$3" ]; then
+			echo "$1"
+		elif [ ${#2} -eq "1" ]; then
+			echo "$1 -$2 $3"
+		else
+			echo "$1 --$2=$3"
+		fi
+	elif [[ "$2" == "resume" || \
+			"$2" == "ganglia" || \
+			"$2" == "no-ganglia" || \
+			"$2" == "delete-groups" || \
+			"$2" == "use-existing-master" ]]; then
+		echo "$1 --$2"
+	else
+		echo "$1"
+	fi
+}
+
+if [[ -z "$KEY_PAIR_NAME" || -z "$KEY_FILE" ]]; then
+
+	echo "You have not chosen a key pair or file. Hardcode them into the script to avoid seeing this prompt again."
+	
+	echo -e "Enter the name of your AWS ec2 key pair: "
+	read KEY_PAIR_NAME
+	
+	echo -e "Enter the path to your key file: "
+	read KEY_FILE
+fi
+
 
 # environment variables are not set
 if [[ -z "$AWS_ACCESS_KEY_ID" || -z "$AWS_SECRET_ACCESS_KEY" ]]; then
@@ -34,8 +83,53 @@ while true; do
 		echo -e "Enter cluster-name: "
 		read cluster_name
 
+		base_cmd="spark-ec2 -asdsad "bad_arg" -k $KEY_PAIR_NAME -i $KEY_FILE -s $num_slaves launch $cluster_name"
+		while true; do
+			echo -e "Would you like to add any more flags (y/n)?"
+			read ans
+			if [ $ans == "y" ]; then
+				echo -e "Which flag would you like to set? \n \
+					w (wait time) \n \
+					t (instance type) \n \
+					m (master instance type) \n \
+					r (region) \n \
+					z (zone) \n \
+					a (Amazon Machine Image) \n \
+					v (spark version) \n \
+					D (SSH dynamic port forwaring) \n \
+					u (user - root is default) \n \
+					spark-git-repo \n \
+					hadoop-major-version \n \
+					ebs-vol-size \n \
+					swap \n \
+					spot-price \n \
+					worker-instances \n \
+					master-ops \n \
+					resume \n \
+					ganglia \n \
+					no-ganglia \n \
+					delete-groups \n \
+					use-existing-master"
+				read flag
+				if [[ "$flag" == "resume" || \
+					"$flag" == "ganglia" || \
+					"$flag" == "no-ganglia" || \
+					"$flag" == "delete-groups" || \
+					"$flag" == "use-existing-master" ]]; then
+					base_cmd=$(add_param "$base_cmd" "$flag")
+				else
+					echo -e "What value do you want to set for $flag?"
+					read val
+					base_cmd=$(add_param "$base_cmd" "$flag" "$val")
+				fi
+			elif [ $ans == "n" ]; then
+				break
+			fi
+		done
+
 		echo "Starting cluster \"$cluster_name\" with $num_slaves slaves..."
 		source spark-ec2 -asdsad "bad_arg" -k $KEY_PAIR_NAME -i $KEY_FILE -s $num_slaves launch $cluster_name
+
 	elif [[ "$command" == "destroy" || "$command" == "stop" ]]; then
 		echo -e "Enter cluster-name: "
 		read cluster_name
